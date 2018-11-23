12cad3e04467b56bf3f76139a5df452fa0579b66
Andrey Felinger <akaincore@gmail.com>
2018-11-23 22:34:51 +0700

    add material-ui, add layout components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import DrawerLeft from "./components/layout/Drawer";
+import Header from "./components/layout/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Chat</h1>
+        <Header/>
+        <DrawerLeft/>
       </div>
     );
   }
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,29 @@
+import AppBar from "@material-ui/core/AppBar/index";
+import React from 'react';
+import Toolbar from "@material-ui/core/es/Toolbar/Toolbar";
+import Typography from "@material-ui/core/es/Typography/Typography";
+import {withStyles} from '@material-ui/core/styles';
+
+
+const styles = () => ({
+  appBar: {
+    width: `calc(100% - 320px)`,
+    marginLeft: 320,
+  },
+});
+
+function Header(props) {
+  const {classes} = props;
+
+  return (
+    <AppBar position="static" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" color="inherit">
+          Chat App
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  )
+}
+
+export default withStyles(styles)(Header);
